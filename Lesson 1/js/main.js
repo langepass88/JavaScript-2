23bdcae03c0c92a4ce286c79e881d36b808504e4
langepass88 <blog.langepass@gmail.com>
2019-09-26 17:50:40 +0300

    lesson 1 обновлен

--- Lesson 1/js/main.js
+++ Lesson 1/js/main.js
@@ -1,26 +1,97 @@
 'use strict';
 
-const goods = [
-    {title: 'Компьютерная мышь', price: 400},
-    {title: 'Жетский диск SSD 1Tb', price: 4000},
-    {title: 'Материнская плата', price: 8500},
-    {title: 'Видео-карта', price: 6000},
-    {},
-];
+class ItemsList {
+    constructor() {
+        this.items = [];
+    }
 
-/**
- * Функция генерирует список товара
- * @param items {Array} массив со списком товара.
- */
-const renderList = (items) =>
-    document.querySelector('.catalog').insertAdjacentHTML('afterbegin', items.map(renderItem).join(' '));
+    fetchItems() {
+        this.items = [
+            {title: 'Компьютерная мышь', price: 400},
+            {title: 'Жесткий диск SSD 1Tb', price: 4000},
+            {title: 'Материнская плата', price: 8500},
+            {title: 'Видео-карта', price: 6000},
+            {},
+        ];
+    }
 
-/**
- * Функция, которая генерирует разметку отдельной единицы товара.
- * @param item {Object} объект с характеристиками отдельного товара.
- * @return {string} HTML-разметка товара.
- */
-const renderItem = ({title = 'Unknown title', price = 'Unknown price'}) =>
-    `<div class="product-item"><h3>${title}</h3><p>${price}</p><button class="by-btn">Купить</button></div>`;
+    total() {
+        const sum = this.items.reduce((a, {price = 0}) => a += price, 0);
+        return `Общая сумма всех товаров равна: ${sum} рублей`;
+    }
 
-renderList(goods);
+    render() {
+        return this.items.map((item) => new Item(item.title, item.price).render()).join('');
+    }
+
+}
+
+class Item {
+    constructor(title = 'Unknown Title', price = 'Unknown') {
+        this.title = title;
+        this.price = price;
+    }
+
+    render() {
+        return `<div class="product-item"><h3>${this.title}</h3><p>${this.price}</p><button class="by-btn">Купить</button></div>`;
+    }
+
+}
+
+/*   ПУСТЫЕ КЛАССЫ ДЛЯ КОРЗИНЫ
+class Cart {
+    constructor() {
+        this.cart = [];
+    }
+
+    addToCart() {
+        //добавить товар в корзину
+    }
+
+    totalCart() {
+        // подсчет суммы всех товаров в корзине
+    }
+
+    getShip() {
+        // подсчет суммы доставки
+    }
+
+    totalSum() {
+        // подсчет итоговой суммы с учетом доставки
+    }
+
+    render() {
+        // рендер корзины
+    }
+
+
+}
+
+class CartItem {
+    constructor() {
+        this.title = title; // название товара
+        this.price = price; // цена товара
+        this.count = count; // количество товара
+    }
+
+    moreItem() {
+        //прибавление товара в корзину на 1
+    }
+
+    removeItem() {
+        // удаление товара из корзины
+    }
+
+    render() {
+        // рендер CartItem
+    }
+}
+*/
+
+const items = new ItemsList();
+items.fetchItems();
+document.querySelector('.catalog').insertAdjacentHTML('afterbegin', items.render());
+document.querySelector('.total').insertAdjacentHTML('afterbegin', items.total());
+
+
+
